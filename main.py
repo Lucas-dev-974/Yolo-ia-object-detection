a4c424c0745a345d5ae9d872581db0bc5f2ca2b1
Quentin PAYET <quentinpayet1203@gmail.com>
2023-07-02 13:41:52 +0400

    attempt to use YOLO prediction with streamlit

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import streamlit as st
 from PIL import Image
 
+from ultralytics import YOLO
+
+model = YOLO("./labos/ultralytics/yolov8n.pt")
+
 
 def makePrediction(image):
     return "prediction"
@@ -18,3 +22,5 @@ if uploaded_file is not None:
     st.write("Predicting...")
     prediction = makePrediction(uploaded_file)
     st.write(prediction)
+    result_prediction_test = model(image)
+    st.write(result_prediction_test)
